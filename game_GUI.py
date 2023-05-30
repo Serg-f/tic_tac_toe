d4fe452c950e4519c2a5904dff89e50e68ce7923
Igor <Ayshyama@gmail.com>
2023-05-30 15:13:32 +0200

    new GUI

--- game_GUI.py
+++ game_GUI.py
@@ -1,5 +1,7 @@
 from main_AI import TicTacToe
 import tkinter as tk
+from tkinter import ttk
+import sv_ttk
 
 
 # Define the TicTacToeGUI class
@@ -19,7 +21,7 @@ class TicTacToeGUI:
             self.window.grid_columnconfigure(i, weight=1)
 
         # Create the buttons grid for the Tic Tac Toe board
-        self.buttons = [[tk.Button(self.window, text="_", font='Arial 60', width=2,
+        self.buttons = [[tk.Button(self.window, text="", font='Arial 60', width=2,
                                    command=lambda row=i, col=j: self.click_buttons(row, col))
                          for j in range(3)] for i in range(3)]
 
@@ -28,18 +30,20 @@ class TicTacToeGUI:
             for j in range(3):
                 self.buttons[i][j].grid(row=i, column=j, sticky="nsew")
 
+        sv_ttk.set_theme("dark")  # Set the theme to "dark"
+
         # Create the result label for displaying game outcomes
-        self.result_label = tk.Label(self.window, text="", font='Arial 20')
+        self.result_label = tk.Label(self.window, text="", font='Arial 25')
 
         # Place the result label below the Tic Tac Toe board
         self.result_label.grid(row=3, column=0, columnspan=3)
 
         # Create the reset button for restarting the game
-        self.reset_button = tk.Button(self.window, text="Reset", command=self.reset_game, font='Arial 14',
-                                      bg='lightgray', fg='black', relief=tk.RAISED, width=10)
+        self.reset_button = ttk.Button(
+            self.window, text="Reset", command=self.reset_game, width=10)
 
         # Place the reset button below the result label with padding
-        self.reset_button.grid(row=4, column=1, pady=10)
+        self.reset_button.grid(row=4, column=1, pady=100)
 
         # Configure additional rows and columns to expand
         self.window.grid_rowconfigure(4, weight=1)
@@ -117,7 +121,7 @@ class TicTacToeGUI:
         self.game.reset()
         for i in range(3):
             for j in range(3):
-                self.buttons[i][j]["text"] = "_"
+                self.buttons[i][j]["text"] = ""
                 self.buttons[i][j]["state"] = tk.NORMAL  # Enable the buttons for a new game
         self.result_label["text"] = ""  # Clear the result label
         self.result_shown = False  # Reset the flag to indicate that the result is not shown
